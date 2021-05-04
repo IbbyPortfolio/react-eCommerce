7936efc1ff6d0cf49983f787dc3e99ea0c8a16d2
Ibrahim <ibrahim.s.amin@gmail.com>
2021-05-05 07:56:16 +1000

    initial scaffolding

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Header from './componenets/Header';
 import './default.scss';
+
+import Header from './componenets/Header';
 import Homepage from './pages/Homepage';
 
 const App = (props) => {
--- src/componenets/Directory/index.js
+++ src/componenets/Directory/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
+
 import ShopMen from '../../assets/shopMens.jpg';
 import ShopWomen from '../../assets/shopWomen.jpg';
 import './styles.scss';
 
-const Directory = () => {
+const Directory = (props) => {
    return (
       <div className='directory'>
          <div className='wrap'>
             <div
                className='item'
-               style={{ backgroundImage: `url(${ShopWomen})` }}
+               style={{
+                  backgroundImage: `url(${ShopWomen})`,
+               }}
             >
-               <a>Shop Women</a>
+               <a>Shop Womens</a>
             </div>
-            <div style={{ backgroundImage: `url(${ShopMen})` }}>
-               <a>Shop Men</a>
+            <div
+               className='item'
+               style={{
+                  backgroundImage: `url(${ShopMen})`,
+               }}
+            >
+               <a>Shop Mens</a>
             </div>
          </div>
       </div>
